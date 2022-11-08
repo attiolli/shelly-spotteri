78a1f214ee63d59555d186b37384f45a0194774c
Olli Attila <attiolli@gmail.com>
2022-11-08 11:47:41 +0200

    Virhekäsittelyä korjattu

--- shelly-spotteri.js
+++ shelly-spotteri.js
@@ -13,13 +13,15 @@ let CONFIG = {
 function PatteriOhjaus() {
   Shelly.call("HTTP.GET", { url: CONFIG.patteriUrl},
   function (res, error_code, error_msg, ud) {
-  if (res.code === 200) { // Halpaa
-    Shelly.call("Switch.Set","{ id:0, on:true}", null,null); // Patterirele päälle
-    Shelly.call("Switch.Set","{ id:3, on:false}", null,null); // Lämmöntiputusrele pois päältä
-  }
-  else if (res.code === 400) { // Kallista
-    Shelly.call("Switch.Set","{ id:0, on:true}", null,null); // Patterirele päälle
-    Shelly.call("Switch.Set","{ id:3, on:true}", null,null); // Lämmöntiputusrele päälle
+  if(error_code === 0) {
+      if (res.code === 200) { // Halpaa
+        Shelly.call("Switch.Set","{ id:0, on:true}", null,null); // Patterirele päälle
+        Shelly.call("Switch.Set","{ id:3, on:false}", null,null); // Lämmöntiputusrele pois päältä
+      }
+      else if (res.code === 400) { // Kallista
+        Shelly.call("Switch.Set","{ id:0, on:true}", null,null); // Patterirele päälle
+        Shelly.call("Switch.Set","{ id:3, on:true}", null,null); // Lämmöntiputusrele päälle
+      }
   }
   }, null);
 }
@@ -27,11 +29,13 @@ function PatteriOhjaus() {
 function VaraajaOhjaus() {
   Shelly.call("HTTP.GET", { url: CONFIG.varaajaUrl},
   function (res, error_code, error_msg, ud) {
-  if (res.code === 200) { // Halpaa
-    Shelly.call("Switch.Set","{ id:1, on:true}", null,null); // Varaajarele päälle
-  }
-  else if (res.code === 400) { // Kallista
-    Shelly.call("Switch.Set","{ id:1, on:false}", null,null); // Varaajarele pois
+  if(error_code === 0) {
+    if (res.code === 200) { // Halpaa
+      Shelly.call("Switch.Set","{ id:1, on:true}", null,null); // Varaajarele päälle
+    }
+    else if (res.code === 400) { // Kallista
+      Shelly.call("Switch.Set","{ id:1, on:false}", null,null); // Varaajarele pois
+    }
   }
   }, null);
 }
@@ -39,11 +43,13 @@ function VaraajaOhjaus() {
 function LattiaOhjaus() {
   Shelly.call("HTTP.GET", { url: CONFIG.lattiaUrl},
   function (res, error_code, error_msg, ud) {
-  if (res.code === 200) { // Halpaa
-    Shelly.call("Switch.Set","{ id:2, on:true}", null,null); // Lattialämmitysrele päälle
-  }
-  else if (res.code === 400) { // Kallista
-    Shelly.call("Switch.Set","{ id:2, on:false}", null,null); // Lattialämmitysrele pois
+  if(error_code === 0) {
+    if (res.code === 200) { // Halpaa
+      Shelly.call("Switch.Set","{ id:2, on:true}", null,null); // Lattialämmitysrele päälle
+    }
+    else if (res.code === 400) { // Kallista
+      Shelly.call("Switch.Set","{ id:2, on:false}", null,null); // Lattialämmitysrele pois
+    }
   }
   }, null);
 }
@@ -52,3 +58,27 @@ function LattiaOhjaus() {
 Timer.set(CONFIG.paivitystaajuusPatteri, true, function (ud) { PatteriOhjaus(); }, null);
 Timer.set(CONFIG.paivitystaajuusVaraaja, true, function (ud) { VaraajaOhjaus(); }, null);
 Timer.set(CONFIG.paivitystaajuusLattia, true, function (ud) { LattiaOhjaus(); }, null);
+
+let CONFIG = {
+  paivitystaajuusPatteri:  30000,
+  // Patterit päälle jos kuluva tunti kuuluu 8 halvimman joukkoon TAI jos alle 5 senttiä
+  patteriUrl: "https://api.spot-hinta.fi/JustNowRank/8/5",
+};
+
+function PatteriOhjaus() {
+  Shelly.call("HTTP.GET", { url: CONFIG.patteriUrl},
+  function (res, error_code, error_msg, ud) {
+  if (res.code === 200) { // Halpaa
+    Shelly.call("Switch.Set","{ id:0, on:true}", null,null);
+    Shelly.call("Switch.Set","{ id:1, on:true}", null,null);
+  }
+  else if (res.code === 400) { // Kallista
+    Shelly.call("Switch.Set","{ id:0, on:false}", null,null);
+    Shelly.call("Switch.Set","{ id:1, on:false}", null,null);
+  }
+  }, null);
+}
+
+
+// Ajastusten suoritus
+Timer.set(CONFIG.paivitystaajuusPatteri, true, function (ud) { PatteriOhjaus(); }, null);
