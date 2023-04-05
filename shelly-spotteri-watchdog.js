636644533f791396b7b691d917e76312828f434b
Olli Attila <attiolli@gmail.com>
2023-04-05 08:36:34 +0300

    Shelly spotteri watchdog v2

--- shelly-spotteri-watchdog.js
+++ shelly-spotteri-watchdog.js
@@ -1,25 +1,105 @@
-let stored_errors = [];
-Timer.set(60000, true,
-    function() {
-        Shelly.call(
-            "Script.GetStatus",
-            {id: 1},
-            function(result) {
-                if(result.running === false) {
-                    stored_errors.push({
-                    time: Shelly.getComponentStatus("sys").unixtime,
-                    error: JSON.stringify(result.errors)
-                    });
-                    Shelly.call(
-                        "Script.Start",
-                        {id: 1},
-                    );      
-                }
+/* More information about the API (in Finnish): https://spot-hinta.fi/
+ * Support API development and maintenance: https://www.buymeacoffee.com/spothintafi
+ * 
+ * This script is used to monitor other script's execution. Restarts the scripts if needed.
+ * Scripts containing HTTP requests can fail so this script will notice it and restart.
+ * Also Internet connection can be monitored and Shelly rebooted if connection is lost. */
+
+// *****************************
+// SETTINGS
+// *****************************
+
+let SETTINGS = {
+    MonitorScript_1: true,  // True/false, depending if you want to monitor script 1
+    MonitoredScript_1: "1", // ID of the script to be monitored
+    MonitorScript_2: false,  // True/false, depending if you want to monitor script 2
+    MonitoredScript_2: "2", // ID of the other script to be monitored
+    MonitorInternetConnection: true,  // True = reboot Shelly if internet connection is lost
+    NumberOfFailedRoundsToRebootShelly: 5,  // How many timer rounds will be waited before reboot. 
+};
+
+// **************************************
+// MAIN SCRIPT - NO NEED TO TOUCH (USUALLY)
+// **************************************
+
+// Default timer round is 60 seconds
+Timer.set(60000, true, function (ud) {
+    MonitorScriptExecution();
+    MonitorInternetConnection();
+}, null); 
+
+
+function MonitorScriptExecution()
+{
+    // Monitoring script 1
+    if (SETTINGS.MonitorScript_1 === true)
+    {
+        print("Monitoring script 1 execution, script ID to monitor: " + SETTINGS.MonitoredScript_1);
+
+        Shelly.call("Script.GetStatus", { id: SETTINGS.MonitoredScript_1 }, function (res)
+        {
+            if (res.running === true)
+            {
+                print("Monitored script 1 is running. All good.");
             }
-        );
+            else
+            {
+                print("Script 1 is not running. Starting the script.");
+                Shelly.call("Script.Start", { id: SETTINGS.MonitoredScript_1 }, null, null);
+                Shelly.call("Script.SetConfig", { id: SETTINGS.MonitoredScript_1, config: {enable:true}}, null, null);
+            }
+        }, null);
+    }
 
-if(stored_errors.length > 0) {
-print("Shelly 4pm tarkistaja: ", JSON.stringify(stored_errors))
+    // Monitoring script 2
+    if (SETTINGS.MonitorScript_2 === true)
+    {
+        print("Monitoring script 2 execution, script ID to monitor: " + SETTINGS.MonitoredScript_2);
 
+        Shelly.call("Script.GetStatus", { id: SETTINGS.MonitoredScript_2 }, function (res)
+        {
+            if (res.running === true)
+            {
+                print("Monitored script 2 is running. All good.");
+            }
+            else
+            {
+                print("Script 2 is not running. Starting the script.");
+                Shelly.call("Script.Start", { id: SETTINGS.MonitoredScript_2 }, null, null);
+                Shelly.call("Script.SetConfig", { id: SETTINGS.MonitoredScript_2, config: {enable:true}}, null, null);
+            }
+        }, null);
+    }
 }
-})
+
+// Monitor Internet Connection
+let failedRounds = 0;
+function MonitorInternetConnection()
+{
+    if (SETTINGS.MonitorInternetConnection === false) {
+        return;
+    }
+
+    Shelly.call("Shelly.GetStatus", "", function (res)
+    {
+
+        // TODO: are there better ways to check the Internet connection?
+        if (res.wifi.status === "got ip" && res.cloud.connected === true)
+        {
+            failedRounds = 0;
+            print("Internet connection is working and connected to the Shelly cloud");
+            return;
+        }
+
+        failedRounds += 1;
+        print("Internet connection is NOT working. Failed rounds now:");
+        print(failedRounds);
+
+        if (failedRounds >= SETTINGS.NumberOfFailedRoundsToRebootShelly)
+        {
+            failedRounds = 0;
+            print("Rebooting Shelly because Internet connection does not work!");
+            Shelly.call("Shelly.Reboot");
+        }
+    }, null);
+}
